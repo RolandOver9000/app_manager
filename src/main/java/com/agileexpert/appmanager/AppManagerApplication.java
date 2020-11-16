cb50923d0559998a52969adec7a234ac3e126928
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-16 16:22:14 +0100

    project initialized

---
+++ src/main/java/com/agileexpert/appmanager/AppManagerApplication.java
@@ -0,0 +1,13 @@
+package com.agileexpert.appmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppManagerApplication.class, args);
+    }
+
+}

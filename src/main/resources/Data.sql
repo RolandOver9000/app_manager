0dd46b0b7e31ada07167cfcf11f9896d05ab0d59
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-20 00:22:03 +0100

    created dummy data in resources directory

--- src/main/resources/Data.sql
+++
@@ -1,5 +0,0 @@
-insert into family
-values(20, 10)
-
-insert into app_manager_user
-values(10, true, 'pwwww', 'Roland', 20)
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into family
+values(10)
+
+insert into app_manager_user
+values(10, 1, 'pw', 'Roland', 20)

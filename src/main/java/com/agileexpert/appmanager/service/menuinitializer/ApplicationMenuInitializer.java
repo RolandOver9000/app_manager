80f57335f169f00235cfcb8db99367eb3ffb6dc9
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-19 20:32:11 +0100

    created an initializer for application menu setup

---
+++ src/main/java/com/agileexpert/appmanager/service/menuinitializer/ApplicationMenuInitializer.java
@@ -0,0 +1,28 @@
+package com.agileexpert.appmanager.service.menuinitializer;
+
+import com.agileexpert.appmanager.model.menuelement.MenuElement;
+import com.agileexpert.appmanager.model.menuelement.navigationelement.MenuNavigationElement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationMenuInitializer extends MenuInitializer{
+
+    private final InstalledApplications installedApplications;
+    private Map<String, MenuElement> subElements = new HashMap<>();
+
+    @Override
+    public void initialize() {
+        menuElement = MenuNavigationElement.builder()
+                .menuElementName("Application menu")
+                .build();
+    }
+
+    @Override
+    public void linkMenuElements() {
+    }
+}

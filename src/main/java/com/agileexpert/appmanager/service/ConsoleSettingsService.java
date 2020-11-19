822130301e26bcc936b15e810a5099459be1f027
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-19 20:14:05 +0100

    handled icon deletion in the database

--- src/main/java/com/agileexpert/appmanager/service/ConsoleSettingsService.java
+++ src/main/java/com/agileexpert/appmanager/service/ConsoleSettingsService.java
@@ -33,13 +33,11 @@ public class ConsoleSettingsService {
     }
 
     public boolean trySetNewIcon(String newIcon) {
-        Optional<ConsoleSettings> currentConsoleSettings = consoleSettingsRepository
-                        .getConsoleSettingsByAppManagerUser(appManagerContext.getCurrentAppManagerUser());
-        if(currentConsoleSettings.isPresent()) {
-            ConsoleSettings searchedSettings = currentConsoleSettings.get();
+        ConsoleSettings searchedSettings = getCurrentConsoleSettings();
+        if(searchedSettings != null) {
             searchedSettings.setIcon(newIcon);
             consoleSettingsRepository.save(searchedSettings);
-            refreshAppManagerContextCurrentConsoleSettings(currentConsoleSettings.get());
+            refreshAppManagerContextCurrentConsoleSettings(searchedSettings);
             return true;
         }
         return false;
@@ -47,5 +45,17 @@ public class ConsoleSettingsService {
 
     private void refreshAppManagerContextCurrentConsoleSettings(ConsoleSettings currentConsoleSettings) {
         appManagerContext.setCurrentConsoleSettings(currentConsoleSettings);
+    }
+
+    public void removeCurrentUserIcon() {
+        ConsoleSettings changedConsoleSettings = getCurrentConsoleSettings();
+        changedConsoleSettings.setIcon("");
+        refreshAppManagerContextCurrentConsoleSettings(changedConsoleSettings);
+    }
+
+    private ConsoleSettings getCurrentConsoleSettings() {
+        Optional<ConsoleSettings> currentConsoleSettings = consoleSettingsRepository
+                .getConsoleSettingsByAppManagerUser(appManagerContext.getCurrentAppManagerUser());
+        return currentConsoleSettings.orElse(null);
     }
 }
